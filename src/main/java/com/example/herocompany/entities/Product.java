fbef9f9e880740e8aed51ce4f8821fc9c505cd68
burotti1 <burotti1@gmail.com>
2022-11-14 17:46:19 +0300

    Initial commit

--- src/main/java/com/example/herocompany/entities/Product.java
+++ src/main/java/com/example/herocompany/entities/Product.java
@@ -19,18 +19,18 @@ public class Product {
     private Long id;
 
     @NotBlank(message = "Detail cant be blank")
-    @Length(message = "Product name must contain max  50 character.",max=50)
+    @Length(message = "Product name must contain max  50 character.", max = 50)
     private String productName;
 
     @NotBlank(message = "Detail cant be blank")
-    @Length(message = "Product detail must contain max  500 character.",max=500)
+    @Length(message = "Product detail must contain max  500 character.", max = 500)
     private String detail;
 
-    @Range(message = "price can be between 5 and 9999",min = 5,max = 9999)
+    @Range(message = "price can be between 5 and 9999", min = 5, max = 9999)
     private int price;
 
     @ManyToOne
-    @JoinColumn(name = "category_id",referencedColumnName = "id")
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
 
     @ManyToMany(fetch = FetchType.LAZY,
