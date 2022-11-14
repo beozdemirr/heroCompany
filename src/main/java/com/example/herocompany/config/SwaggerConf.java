96f7ca222f0e571537a3526552e8be3d78d38f61
burotti1 <burotti1@gmail.com>
2022-11-14 10:34:32 +0300

    Initial commit

--- src/main/java/com/example/herocompany/config/SwaggerConf.java
+++ src/main/java/com/example/herocompany/config/SwaggerConf.java
@@ -2,7 +2,6 @@ package com.example.herocompany.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
