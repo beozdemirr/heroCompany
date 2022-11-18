1aa406c82b796259361ba7a79120c0cb06dde6b2
burotti1 <burotti1@gmail.com>
2022-11-18 09:53:47 +0300

    Initial commit

--- src/main/java/com/example/herocompany/entities/Attribute.java
+++ src/main/java/com/example/herocompany/entities/Attribute.java
@@ -1,6 +1,7 @@
 package com.example.herocompany.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -15,6 +16,7 @@ public class Attribute {
     private Long id;
     private String feature;
 
+    @JsonIgnore
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL,mappedBy = "attributes")
     private List<Product> products;
 }
